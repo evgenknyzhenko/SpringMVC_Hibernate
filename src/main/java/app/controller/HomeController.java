9242cf355b92a329b514bbd4f0ec6dfa04b8a660
evgenknyzhenko <evgen.knyzhenko@gmail.com>
2018-07-13 15:40:29 +0300

    Lesson3.1

--- src/main/java/app/controller/HomeController.java
+++ src/main/java/app/controller/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
+
+
     public String homePage() {
         return "home";
     }
